b503091bc5928c4346f38fc898e5e69f9a382fa6
Vanessa Millan <71412427+VaneMG@users.noreply.github.com>
2023-05-16 20:32:24 -0800

    Add files via upload

--- KY-011 (Two-Color)/KY-011 (Two-Color).py
+++ KY-011 (Two-Color)/KY-011 (Two-Color).py
@@ -1,1 +1,12 @@
+from machine import Pin
+import time
 
+led_pins = [14,15]
+leds = [Pin(led_pins[0],Pin.OUT),Pin(led_pins[1],Pin.OUT)]
+delay_t = 0.1
+while True:
+    for led in leds:
+        led.high()
+        time.sleep(delay_t)
+        led.low()
+        time.sleep(delay_t)
