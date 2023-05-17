17041656dffdd61a33a978bc34b04d85f9797a69
Vanessa Millan <71412427+VaneMG@users.noreply.github.com>
2023-05-16 20:25:10 -0800

    Add files via upload

--- KY-004 (BUTTON)/KY-004 (BUTTON).py
+++ KY-004 (BUTTON)/KY-004 (BUTTON).py
@@ -1,1 +1,12 @@
+from machine import Pin
+from time import sleep
 
+button_pin = Pin(16, Pin.IN)
+
+while True:
+    button_state = button_pin.value()
+
+    if button_state == 0:
+        print("Button pressed")
+
+    sleep(0.1)
