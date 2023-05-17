1857d66dc2006c6452132df9bf72364459199a6d
Vanessa Millan <71412427+VaneMG@users.noreply.github.com>
2023-05-16 20:44:42 -0800

    Add files via upload

--- KY-021 (Mini Switch)/KY-021 (Mini Switch).py
+++ KY-021 (Mini Switch)/KY-021 (Mini Switch).py
@@ -1,1 +1,15 @@
+from machine import Pin
+import utime
 
+pin=16
+sensor=Pin(pin, Pin.IN)
+utime.sleep(1)
+
+while True:
+    if sensor.value()==1:
+        print("Switch Apagado")
+        utime.sleep(1)    
+    else:
+        print("Switch Encendido")
+        utime.sleep(1)
+utime.sleep(1)
