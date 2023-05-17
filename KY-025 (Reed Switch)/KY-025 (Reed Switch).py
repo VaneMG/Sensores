423f78cb61b81607a7762993bb73c3f6fdbdb9e4
Vanessa Millan <71412427+VaneMG@users.noreply.github.com>
2023-05-16 20:47:45 -0800

    Add files via upload

--- KY-025 (Reed Switch)/KY-025 (Reed Switch).py
+++ KY-025 (Reed Switch)/KY-025 (Reed Switch).py
@@ -1,1 +1,12 @@
+from machine import Pin
+import time
 
+ReedSensor = Pin(18, Pin.IN)
+while True:
+    value = ReedSensor.value()
+    print(value, end = " ")
+    if value == 0:
+        print("Hay campo magnetico")
+    else:
+        print("No hay campo magnetico")
+    time.sleep(0.1)
