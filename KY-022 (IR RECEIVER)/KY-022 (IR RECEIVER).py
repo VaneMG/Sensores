76cfcad8d7a09769ab68668f01ef51ae25789185
Vanessa Millan <71412427+VaneMG@users.noreply.github.com>
2023-05-16 20:46:33 -0800

    Add files via upload

--- KY-022 (IR RECEIVER)/KY-022 (IR RECEIVER).py
+++ KY-022 (IR RECEIVER)/KY-022 (IR RECEIVER).py
@@ -1,1 +1,18 @@
+from machine import Pin
+import time
 
+pico_led = Pin(25, Pin.OUT)
+ir = Pin(15, Pin.OUT)
+receiver = Pin(16, Pin.IN)
+
+
+while True:
+    ir.value(1)
+    
+    #Cuando el sensor recibe un valor se prende el led de la Pico
+    if(receiver.value() == 1):
+        pico_led.value(1)
+    else:
+        pico_led.value(0)
+        
+    time.sleep(1)
