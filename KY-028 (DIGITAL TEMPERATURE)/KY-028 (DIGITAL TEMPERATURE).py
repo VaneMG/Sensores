bf28c6751a5365928bac28aabb8dfe65cdcd475d
Vanessa Millan <71412427+VaneMG@users.noreply.github.com>
2023-05-22 16:24:08 -0800

    Add files via upload

--- KY-028 (DIGITAL TEMPERATURE)/KY-028 (DIGITAL TEMPERATURE).py
+++ KY-028 (DIGITAL TEMPERATURE)/KY-028 (DIGITAL TEMPERATURE).py
@@ -1,1 +1,27 @@
-
+import machine
+import time
+ 
+# Configuración del pin de entrada de datos del KY-028
+sensor_pin = machine.ADC(26)
+ 
+# Función para convertir la lectura del sensor en grados Celsius
+def convert_to_celsius(reading):
+    millivolts = (reading * 3300) / 65535
+    celsius = (millivolts - 300) / 10
+    if celsius < -40:
+        celsius = -40
+    elif celsius > 125:
+        celsius = 125
+    return celsius
+ 
+# Bucle principal para leer la temperatura cada segundo
+while True:
+    # Leer el valor del sensor y convertirlo a grados Celsius
+    reading = sensor_pin.read_u16()
+    celsius = convert_to_celsius(reading)
+ 
+    # Imprimir la temperatura en la consola
+    print("Temperatura actual: {:.2f} C".format(celsius))
+ 
+    # Esperar un segundo antes de volver a leer la temperatura
+    time.sleep(1)
