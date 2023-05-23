f9f70aeeb1624eef2749593a208e39eb84d2cc3b
Vanessa Millan <71412427+VaneMG@users.noreply.github.com>
2023-05-22 16:15:50 -0800

    Add files via upload

--- KY-035 (ANALOG HALL)/KY-035 ANALOG HALL.py
+++ KY-035 (ANALOG HALL)/KY-035 ANALOG HALL.py
@@ -1,1 +1,11 @@
+from machine import Pin
+import time
 
+iman=Pin(14,Pin.IN)
+
+while True:
+    if iman.value() == 0:
+        print("Campo detectado")
+    elif iman.value()==1:
+        print("No se detecta un campo")
+    time.sleep(1)
